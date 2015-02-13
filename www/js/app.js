001b8cb9198e60231de640233deb21053688b5e1
Ian <iantskon@gmail.com>
2015-02-13 15:27:38 -0800

    Remove scaffolded files

--- www/js/app.js
+++ www/js/app.js
@@ -24,7 +24,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 // Configure backend api for auth provider
 .config(function($authProvider) {
         $authProvider.configure({
-            apiUrl: 'https://qr3backend.herokuapp.com/'
+            apiUrl: 'https://qr3backend.herokuapp.com'
         });
     })
 
@@ -51,64 +51,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       'tab-index': {
         templateUrl: 'templates/tab-index.html',
         controller: 'IndexCtrl'
-      }
-    }
-  })
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
       }
     }
   });
