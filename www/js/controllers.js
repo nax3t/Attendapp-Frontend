f10202d1b4d24b7eb8e14b8fc4e227d7a29245a5
Ian <iantskon@gmail.com>
2015-02-13 19:49:16 -0800

    Update controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('IndexCtrl', function($scope, $auth) {
+.controller('IndexCtrl', function($scope, $auth, $location) {
   $scope.register = function() {
     $auth.submitRegistration($scope.user)
       .then(function(resp) { 
-        console.log("*************");
-        console.log(resp);
-        alert(resp);
+        $auth.submitLogin({
+          email: $scope.user.email,
+          password: $scope.user.password
+        });
+        console.dir(resp.config.data.email);
       })
       .catch(function(resp) { 
         // handle error response
